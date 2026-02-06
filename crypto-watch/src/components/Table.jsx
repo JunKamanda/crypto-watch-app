3b1ead077123eaeb8730abf9b973207b07a9a3b0
JunKamanda <jkamanda062@gmail.com>
2026-02-06 15:38:32 +0100

    feat: ajouter la gestion de l'ordre et de la plage dans le composant Table

--- crypto-watch/src/components/Table.jsx
+++ crypto-watch/src/components/Table.jsx
@@ -1,9 +1,65 @@
 import React from "react";
+import { useState } from "react";
 
 const Table = ({ coinsData }) => {
+  //STATE
+  const [orderBy, setOrderBy] = useState("");
+  const [rangeNumber, setRangeNumber] = useState(100);
+
+  //COMPORTEMENT
+  const tableHeader = [
+    "Prix",
+    "MarketCap",
+    "Volume",
+    "1h",
+    "1j",
+    "1m",
+    "6m",
+    "1a",
+    "ATH",
+  ];
+
   return (
-    <div>
-      <h1>TABLE</h1>
+    <div className="table-container">
+      <div className="table-header">
+        <div className="range-container">
+          <span>
+            Top {""}
+            <input
+              type="text"
+              value={rangeNumber}
+              onChange={(e) => setRangeNumber(e.target.value)}
+            />
+          </span>
+          <input
+            type="range"
+            min="1"
+            max="250"
+            value={rangeNumber}
+            onChange={(e) => setRangeNumber(e.target.value)}
+          />
+        </div>
+        {tableHeader.map((el) => (
+          <li key={el}>
+            <input
+              type="radio"
+              name="header-el"
+              id={el}
+              defaultChecked={
+                el === orderBy || el === orderBy + "reverse" ? true : false
+              }
+              onClick={()=>{
+                if(orderBy === el){
+                    setOrderBy(el + "reverse")
+                } else {
+                    setOrderBy(el)
+                }
+              }}
+            />
+            <label htmlFor={el}>{el}</label>
+          </li>
+        ))}
+      </div>
     </div>
   );
 };
